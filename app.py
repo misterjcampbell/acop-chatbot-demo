1fe9ff7118964cf8b00c0800da9846b46ba29e52
Jonno <116990072+misterjcampbell@users.noreply.github.com>
2025-11-28 16:27:32 +1100

    Update app run configuration for production
    Change app to run on specified port and disable debug mode.

--- app.py
+++ app.py
@@ -136,5 +136,6 @@ def book():
 # RUN APP
 # -------------------------
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
